ff10b58a6cd14ae57f4302baf2a2ed3fa58b9f35
Haikyuu Katzuki <114855330+Haikyuu504@users.noreply.github.com>
2024-07-30 13:11:45 +0800

    Add files via upload

---
+++ alive.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+function startServer() {
+  http
+    .createServer(function (req, res) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      var htmlContent = ""; // HTML content removed
+
+      res.write(htmlContent);
+      res.end();
+    })
+    .listen(9000, () =>
+      console.log("HTTP server running on http://localhost:9000")
+    );
+}
+
+module.exports = { startServer };
